a7dbb40ec5b35b257829edb9d8e57afa5627328f
Himanshu Sah <himanshu.sah@delostylestudio.com>
2024-08-23 18:23:10 +0530

    learn jsx and components

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from "react"
 import ReactDOM from 'react-dom/client'
 
+// React element 
 const heading = React.createElement(
-    'h1',
-    {id:'heading'},
-    'First heading'
+    'h4',
+    {id: 'heading'},
+    'Namaste React!'
+)
+
+// JSX
+const JSX = <h1 id="heading ">Namaste React using JSX </h1>
+
+// Functional Component
+const Title = () => (
+    <>
+        <h1>this is title component</h1>
+        {heading}
+    </>
+)
+
+const FirstComponent = () => (
+    <div id="container">
+        <h1>Namaste React Functional Component</h1>
+        <Title/>
+    </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+root.render(<FirstComponent />)
