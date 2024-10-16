d72d179dede2e758d6022fa2249f997899d77d04
Himanshu Sah <09himanshusah@gmail.com>
2024-10-16 20:18:03 +0530

    learn useState, useEffect

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,18 +1,53 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { ResturantsContainers } from './Resturants'
 import resturant from '../utils/mocData'
+import Shimmer from './Shimmer'
 
 
 const Body = () => {
-  const [restuarent, setRestuarent] = useState(resturant)
+  const [restuarent, setRestuarent] = useState([])
+  const [search, setSearch] = useState('')
   
-  return (
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    setTimeout(() => {
+      setRestuarent(resturant)
+    }, 400)
+  }
+
+  return !restuarent.length ? (
+    <h1>No data found</h1>
+  ) : (
     <div className="body">
       <div className="search">
-        <button className="filter-btn" onClick={() => {
-          let filter = resturant.filter((ele) => ele.rating > 4.5)
-          setRestuarent(filter)
-        }}>
+        <div className="search_resturant">
+          <input
+            type="text"
+            name="text"
+            id=""
+            className="search_box"
+            value={search}
+            onChange={(ele) => {
+              setSearch(ele.target.value)
+            }}
+          />
+          <button className="search_food" onClick={() => {
+            let regex = new RegExp(search, 'i')
+            let array = resturant.filter(ele => regex.test(ele.food_name))
+            // if(!array.length) return setRestuarent(<h1>No such resturant avaliable </h1>)
+            setRestuarent(array)
+          }}>Search</button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let filter = resturant.filter((ele) => ele.rating > 4.5)
+            setRestuarent(filter)
+          }}
+        >
           Top rated restuarant
         </button>
       </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,29 @@
+import {useState} from 'react'
 import image from '../images/Designer.png'
 
-const Header = () => (
-  <div className="header">
-    <div className="logo_section">
-      <img src={image} alt="logo" />
+const Header = () => { 
+  let btn_name = 'Log in'
+  let [btn, set_btn] = useState(btn_name)
+
+  const onclick = async () => {
+    btn == btn_name ? set_btn('Logout') : set_btn(btn_name)
+  }
+  return (
+    <div className="header">
+      <div className="logo_section">
+        <img src={image} alt="logo" />
+      </div>
+      <div className="menu">
+        <ul className="nav-item">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button className="login_btn" onClick={onclick}>{btn}</button>
+        </ul>
+      </div>
     </div>
-    <div className="menu">
-      <ul className="nav-item">
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
-)
+  )
+}
 
 export {Header}
